afa7ba04dc0310f028bf64dec72b22a333cdc06b
CowsmonDev <agustin.crespo22@gmail.com>
2020-11-03 21:26:48 -0300

    Modificacion del selectMode y creacion del codigo del exitPlay

--- pasapalabra.pas
+++ pasapalabra.pas
@@ -1,6 +1,6 @@
 program pasapalabras;
 
-//Declaraciones:
+//Declaraciones De Tipos y Constantes:
 
 	//Jugadores:
 		type typeJugador = record
@@ -48,33 +48,99 @@ program pasapalabras;
 
 //Metodos Generales:
 
-function isExists(var arch : archJugador; Direccion : String): boolean;
-begin
-	assign(arch,Direccion);
-	{$I-}
-		reset(arch);
-	{$I+}
-	isExists := (IOResult = 0);
-end;
-
-procedure isExists(var arch : archPalabras; Direccion, aviso : String);
-begin
-	assign(arch,Direccion);
-	{$I-}
-		reset(arch);
-	{$I+}
-	if IOResult <> 0 then begin
-		writeln(aviso);
-		exit
+	function isExists(var Jugador : archJugador; Direccion : String): boolean;
+	begin
+		assign(Jugador,Direccion);
+		{$I-}
+			reset(Jugador);
+		{$I+}
+		isExists := (IOResult = 0);
 	end;
-end;
+
+	procedure isExists(var Palabras: archPalabras; Direccion, aviso : String);
+	begin
+		assign(Palabras,Direccion);
+		{$I-}
+			reset(Palabras);
+		{$I+}
+		if IOResult <> 0 then begin
+			writeln(aviso);
+			HALT;
+		end;
+	end;
+
+	function writeMenu(): String;
+	begin
+		writeln('----------------');
+		writeln('| PASAPALABRAS |');
+		writeln('----------------');
+		writeln('1. Agrega un jugador');
+		writeln('2. Ver lista de jugadores');
+		writeln('3. Jugar');
+		writeln('4. Salir');
+		writeln('');
+		write('Selecciona una opcion: '); Readln(writeMenu);
+	end;
 
 //Metodos:
+	//Agregar un jugador:
 
+		procedure addPlayer();
+		begin
+
+		end;
+
+	//Listar jugadores
+		procedure listPayers();
+		begin
+
+		end;
+
+	//Jugar:
+		procedure play();
+		begin
+
+		end;
+
+	//Salir:
+		procedure exitPlay(var Palabras : archPalabras; var Jugador : archJugador);
+		begin
+			writeln('');
+			writeln('--------------------------------------------');
+			writeln('| Esta saliendo del juego vuelva pronto!!! |');
+			writeln('--------------------------------------------');
+			writeln('');
+			close(Palabras);
+			close(Jugador);
+			HALT;
+		end;
+
+	procedure selectMode(var Palabras : archPalabras; var Jugador : archJugador);
+		var Mode : String;
+	begin
+		Mode :=  writeMenu();
+		while(Mode <> '4') do begin
+			Case Mode of
+				'1' : addPlayer();
+				'2' : listPayers();
+				'3' : play();
+				else begin
+					writeln('Esa Opcion no existe');
+					writeln('');
+					writeln('////////\\\\\\\\');
+					writeln('');
+				end;
+			end;
+			Mode := writeMenu();
+		end;
+		exitPlay(Palabras,Jugador);
+	end;
+
+//variables:
 var Palabras : archPalabras;
 var Jugador : archJugador;
-
 begin
 	if not isExists(Jugador,'ip2/Acrespo-Jugadores.dat') then rewrite(Jugador);
 	isExists(Palabras,'ip2/palabras.dat','Error: lo sentimos, pero el archivo Palabras no se pudo abrir');
+	selectMode(Palabras,Jugador);
 end.
